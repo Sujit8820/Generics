0b4b26a82caa8712412127dbefc79b52d3b56bf0
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-22 22:40:34 +0530

    [Sujit] ADDED: Find the maximum integer from given 3 intergers

---
+++ Max.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Max
+    {
+        public int max = 0;
+
+        public void MaxInt(int firstNo, int secondNo, int thirdNo)
+        {
+            if (firstNo.CompareTo(secondNo) > 0 && firstNo.CompareTo(thirdNo) > 0 ||
+               firstNo.CompareTo(secondNo) >= 0 && firstNo.CompareTo(thirdNo) > 0 ||
+               firstNo.CompareTo(secondNo) > 0 && firstNo.CompareTo(thirdNo) >= 0)
+            {
+                max = firstNo;
+            }
+            if (secondNo.CompareTo(firstNo) > 0 && secondNo.CompareTo(thirdNo) > 0 ||
+               secondNo.CompareTo(firstNo) >= 0 && secondNo.CompareTo(thirdNo) > 0 ||
+               secondNo.CompareTo(firstNo) > 0 && secondNo.CompareTo(thirdNo) >= 0)
+            {
+                max = secondNo;
+            }
+            if (thirdNo.CompareTo(firstNo) > 0 && thirdNo.CompareTo(secondNo) > 0 ||
+               thirdNo.CompareTo(firstNo) >= 0 && thirdNo.CompareTo(secondNo) > 0 ||
+               thirdNo.CompareTo(firstNo) > 0 && thirdNo.CompareTo(secondNo) >= 0)
+            {
+                max = thirdNo;
+            }
+            Console.WriteLine($"\nMaximum value {max}");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,19 @@ namespace Generics
         
         public static void Main(string[] args)
         {
-            Console.WriteLine("Wellcome to Generics Program");
+            Console.WriteLine("Enter three value\n");
+            Console.Write("First value:");
+            int firstNo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Second value:");
+            int SecondNo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Third value:");
+            int ThirdNo = Convert.ToInt32(Console.ReadLine());
+
+            Max max = new Max();
+            max.MaxInt(firstNo, SecondNo, ThirdNo);
+
         }
     }
 }
