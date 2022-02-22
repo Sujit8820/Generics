2f9f9637bd47cb12a30c3a9f2855176f1e8a7227
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-22 22:30:55 +0530

    [Sujit] ADDED: Test Maximum

--- Program.cs
+++ Program.cs
@@ -8,20 +8,7 @@ namespace Generics
         
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter three value\n");
-            Console.Write("First value:");
-            int firstNo =Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Second value:");
-            int SecondNo = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Third value:");
-            int ThirdNo = Convert.ToInt32(Console.ReadLine());
-
-            Max max = new Max();
-            max.MaxInt(firstNo, SecondNo, ThirdNo);
-
-
+            Console.WriteLine("Wellcome to Generics Program");
         }
     }
 }
