74246ff9967d0a0a39febc93c8c3994e258384b7
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-22 22:17:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Generics
+{
+    class program
+    {
+       
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter three value\n");
+            Console.Write("First value:");
+            int firstNo =Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Second value:");
+            int SecondNo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Third value:");
+            int ThirdNo = Convert.ToInt32(Console.ReadLine());
+
+            Max max = new Max();
+            max.MaxInt(firstNo, SecondNo, ThirdNo);
+
+
+        }
+    }
+}
